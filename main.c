e43010275e837bdf71a718383dbee8bd1c5e655b
Aatir Manzur <aatrapps@gmail.com>
2024-06-22 22:47:18 -0400

    just one frame for now

--- main.c
+++ main.c
@@ -35,14 +35,14 @@ static inline int __i2c_read_blocking(void *i2c_handle, uint8_t addr, uint8_t *d
 // From http://www.paulbourke.net/dataformats/asciiart/
 const char charmap[] = " .:-=+*#%@";
 OV7670_status status;
-	static inline uint8_t bits_packed_per_word(uint8_t pin_count) {
-    	// If the number of pins to be sampled divides the shift register size, we
-    	// can use the full SR and FIFO width, and push when the input shift count
-    	// exactly reaches 32. If not, we have to push earlier, so we use the FIFO
-    	// a little less efficiently.
-    	const uint SHIFT_REG_WIDTH = 32;
-    	return SHIFT_REG_WIDTH - (SHIFT_REG_WIDTH % pin_count);
-	}
+static inline uint8_t bits_packed_per_word(uint8_t pin_count) {
+	// If the number of pins to be sampled divides the shift register size, we
+	// can use the full SR and FIFO width, and push when the input shift count
+	// exactly reaches 32. If not, we have to push earlier, so we use the FIFO
+	// a little less efficiently.
+	const uint SHIFT_REG_WIDTH = 32;
+	return SHIFT_REG_WIDTH - (SHIFT_REG_WIDTH % pin_count);
+}
 
 int main() {
 	stdio_init_all();
@@ -92,40 +92,38 @@ int main() {
 	int sm = 0;
 	uint8_t offset = pio_add_program(CAMERA_PIO, &ov7670_program);
 	dma_channel_config cd = dma_channel_get_default_config(dma_chan);
-	pio_sm_init(CAMERA_PIO, sm, offset, &cd);
+	pio_sm_config c = pio_get_default_sm_config();
+	pio_sm_init(CAMERA_PIO, sm, offset, &c);
 	pio_sm_set_enabled(CAMERA_PIO, sm, true);
-	while (1) {
-		pio_sm_clear_fifos(CAMERA_PIO, sm);
-		pio_sm_restart(CAMERA_PIO, sm);
-		memset(capture_buf, 0, width*height*sizeof(uint8_t));
-		channel_config_set_dreq(&cd, pio_get_dreq(CAMERA_PIO, sm, false));
-		dma_channel_configure(dma_chan, &cd,
+	pio_sm_clear_fifos(CAMERA_PIO, sm);
+	pio_sm_restart(CAMERA_PIO, sm);
+	channel_config_set_dreq(&cd, pio_get_dreq(CAMERA_PIO, sm, false));
+	memset(capture_buf, 0, width*height*sizeof(uint8_t));
+	dma_channel_configure(dma_chan, &cd,
 			capture_buf,        // Destination pointer
 			&pio0->rxf[sm],      // Source pointer
-			height*width, // Number of transfers
+			(height*width)/2, // Number of transfers
 			true                // Start immediately
 			);
 
-		printf ("%s\n", "dump bytes read from d0 to d7*....");
-		sleep_ms(1000);
-		gpio_put(LED_PIN, 1);
-		pio_sm_put_blocking(CAMERA_PIO, sm, width);
-		pio_sm_put_blocking(CAMERA_PIO, sm, height);
-		sleep_ms(3000);
-		gpio_put(LED_PIN, 0);
-		sleep_ms(3000);
-		dma_channel_wait_for_finish_blocking(dma_chan);
-		int i;
-		for(i=0;i<height;i++){
+	printf ("%s\n", "dump bytes read from d0 to d7*....");
+	sleep_ms(1000);
+	gpio_put(LED_PIN, 1);
+	pio_sm_put_blocking(CAMERA_PIO, sm, width);
+	pio_sm_put_blocking(CAMERA_PIO, sm, height);
+	sleep_ms(3000);
+	gpio_put(LED_PIN, 0);
+	sleep_ms(3000);
+	int i;
+	for(i=0;i<height;i++){
 
-			for (int j=0;j<width;j++) {
-				printf("%c", pattern[capture_buf[(i*width)+j] /26] );
-				//printf(" byte %d = value: %d\n", i , capture_buf[i]);
-			}
-			printf("%s\n", "");
+		for (int j=0;j<width;j++) {
+			printf("%c", pattern[capture_buf[(i*width)+j] /26] );
+			//printf(" byte %d = value: %d\n", i , capture_buf[i]);
 		}
-		dma_unclaim_mask (dma_chan);
-		
-
+		printf("%s\n", "");
 	}
+	dma_unclaim_mask (dma_chan);
+
+
 }
