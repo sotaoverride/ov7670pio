3375625fc042bf14a8d570da5adcebbec58244d4
Aatir Manzur <aatrapps@gmail.com>
2024-06-23 11:37:18 -0400

    take pio out for now, just get 4 bytes for one line

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "hardware/dma.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 
 #include "camera.h"
@@ -86,50 +87,56 @@ int main() {
 	const uint16_t height = CAMERA_HEIGHT_DIV8;
 	uint8_t dma_chan;
 	dma_chan=dma_claim_unused_channel (true);
-	uint8_t capture_buf [width*height];
+	uint8_t * capture_buf;// [width*height*2];
 
 	uint8_t pattern[] =  " .:!()/|}-=+*#%@";
 	int sm = 0;
 	uint8_t offset = pio_add_program(CAMERA_PIO, &ov7670_program);
 	dma_channel_config cd = dma_channel_get_default_config(dma_chan);
+	channel_config_set_transfer_data_size(&cd, DMA_SIZE_32);
 	pio_sm_config c = pio_get_default_sm_config();
 	pio_sm_init(CAMERA_PIO, sm, offset, &c);
 	pio_sm_set_enabled(CAMERA_PIO, sm, true);
 	pio_sm_clear_fifos(CAMERA_PIO, sm);
 	pio_sm_restart(CAMERA_PIO, sm);
-	channel_config_set_dreq(&cd, pio_get_dreq(CAMERA_PIO, sm, false));
+	pio_sm_set_enabled(pio0, sm, true);
+	//channel_config_set_dreq(&cd, pio_get_dreq(CAMERA_PIO, sm, false));
 	uint8_t * des;
 	uint8_t* source;
 	uint8_t des_v=200;
 	uint8_t source_v=185;
 	des=(uint8_t *)&des_v;
 	source=(uint8_t*)&source_v;
+	/*
 	dma_channel_configure(dma_chan, &cd,
-			des,        // Destination pointer
-			source,      // Source pointer
-			1, // Number of transfers
+			capture_buf,        // Destination pointer
+			&pio0->rxf[sm],      // Source pointer
+			(((width*height*2)/8)), // Number of transfers
 			true                // Start immediately
 			);
 	dma_channel_wait_for_finish_blocking(dma_chan);
 	printf("%d dma check des val should be 185 now??\n", des_v);
+	*/
 	printf ("%s\n", "dump bytes read from d0 to d7*....");
 	sleep_ms(1000);
 	gpio_put(LED_PIN, 1);
-	pio_sm_put_blocking(CAMERA_PIO, sm, width);
-	pio_sm_put_blocking(CAMERA_PIO, sm, height);
+	pio_sm_put_blocking(CAMERA_PIO, sm, 4);
+	pio_sm_put_blocking(CAMERA_PIO, sm, 1);
 	sleep_ms(3000);
 	gpio_put(LED_PIN, 0);
 	sleep_ms(3000);
 	int i;
-	for(i=0;i<height;i++){
-
-		for (int j=0;j<width;j++) {
-			printf("%c", pattern[capture_buf[(i*width)+j] /26] );
+	capture_buf = (uint8_t *) malloc(4);
+	memset(capture_buf, (pio_sm_get_blocking (pio0, sm) ), 4); 
+//	for(i=0;i<height;i++){
+//print the four bytes pushed from the pio
+		for (int j=0;j<32;j++) {
+			printf("%c", pattern[capture_buf[j] /26] );
 			//printf(" byte %d = value: %d\n", i , capture_buf[i]);
 		}
 		printf("%s\n", "");
-	}
-	dma_unclaim_mask (dma_chan);
+//	}
+	//dma_unclaim_mask (dma_chan);
 
 
 }
